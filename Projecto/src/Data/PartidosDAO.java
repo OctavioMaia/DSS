cc5bc5b023a61c1d7ad6c8790a9a99d0d60ba7a7
jms05 <joaosilva@172.26.27.54>
2015-12-04 16:44:43 +0000

    Partidos DAO atualizado atençao depois à ordem colunas na base de dados

--- Projecto/src/Data/PartidosDAO.java
+++ Projecto/src/Data/PartidosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,54 +23,164 @@ import java.util.logging.Logger;
  *
  * @author ruifreitas
  */
-public class PartidosDAO {
-
-    private final String url = "", nome = "root", pass = "senha";
-
-    private Connection con;
-
-    public PartidosDAO(String url, String nome, String pass) {
-        try {
-			this.con = DriverManager.getConnection(url, nome, pass);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+public class PartidosDAO implements Map<Integer,Partido>{
+	private Connector c;
+	
+    public PartidosDAO(Connector c) throws Exception{
+        this.c =c;
     }
-
-    public List<Partido> getPartidos() {
-        try {
-            List<Partido> lista = new ArrayList<Partido>();
-            PreparedStatement s1 = con.prepareStatement("Select * from partidos");
-            ResultSet rs = s1.executeQuery();
-            while(rs.next()){
-                Partido temp = new Partido();
-                temp.setId(Integer.parseInt(rs.getString("id")));
-                temp.setNome(rs.getString("nome"));
-                temp.setSigla(rs.getString("sigla"));
-                temp.setSimbolo(rs.getString("simbolo"));
-                lista.add(temp);
-            }
-            con.commit();
-            return lista;
-        } catch (SQLException ex) {
-            Logger.getLogger(PartidosDAO.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
+	
     
-    public Partido getPartidoId(int id) {
-        try {
-            Partido p = new Partido();
-            PreparedStatement s1 = con.prepareStatement("Select * from partidos where partidos.id=?");
-            s1.setString(1, "" + id);
-            ResultSet res = s1.executeQuery();
-            return p;
-        } catch (SQLException ex) {
-            Logger.getLogger(PartidosDAO.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
+    @Override
+	public int size() {
+		int ret=0;
+    	Connection conn = null;
+    	try{
+    		conn = this.c.newConnection(); 
+    		PreparedStatement ps = conn.prepareStatement("Select  count(*) FROM Partidos");
+    		ResultSet rs = ps.executeQuery();
+    		if(rs.next()) ret = rs.getInt(1);
+    		rs.close();
+    		ps.close();
+    		conn.commit();
+    	}catch(Exception e){
+    		try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+    	}finally{
+    		try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+        return ret; 
+	}
 
+	@Override
+	public boolean isEmpty() {
+		return this.size()==0;
+	}
+
+	@Override
+	public boolean containsKey(Object id) {
+		boolean b=false;
+        Connection conn = null;
+        try{
+        	conn = c.newConnection();
+        	PreparedStatement ps = conn.prepareStatement(" Select  EXISTS (SELECT id FROM Partidos " +
+                " WHERE id = ?)");
+        	ps.setInt(1,(Integer) id);
+        	ResultSet rs = ps.executeQuery();
+        	if (rs.next()) b = (rs.getInt(1)!=0);
+        	rs.close();
+        	ps.close();
+        	conn.commit();
+        }catch(Exception e){
+        	try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        	throw new RuntimeException(e.getMessage());
+        }finally{
+        	try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+        return b;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return this.containsKey(((Partido)value).getId());
+	}
+
+	@Override
+	public Partido get(Object key) {
+		Partido partido  = null;
+        Connection conn = null;
+        
+        try{
+        	conn=this.c.newConnection();
+        	//Atençao ver aqui a ordem das Colunas na Base de dados
+        	PreparedStatement ps = conn.prepareStatement("Select * FROM Partidos WHERE Id = ?");
+        	ps.setInt(1, (Integer)key);
+        	ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+               partido = new Partido (rs.getInt(2),rs.getString(4),rs.getString(1),rs.getString(3));
+            }
+            rs.close();
+            ps.close();
+            conn.commit();
+        }catch(Exception e){
+    		try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+    	}finally{
+    		try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+        return el;
+	}
+
+	@Override
+	public Partido put(Integer key, Partido value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Partido remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends Integer, ? extends Partido> m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Integer> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Partido> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<Integer, Partido>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+    
     
 }
