6d9c1377e3f4440f8d414748bea717df533ab520
PPinto22 <pedropinto24@hotmail.com>
2015-12-04 16:46:01 +0000

    CandidatoAR,Lista

--- Projecto/src/Business/CandidatoAR.java
+++ Projecto/src/Business/CandidatoAR.java
@@ -4,13 +4,29 @@ import java.util.Date;
 
 public class CandidatoAR extends Candidato{
 	
-	private Partido partido;
+	private int partido;
 	private char tipo;
 	
-	public CandidatoAR(String nome, int bi, String prof, Date dataNasc,
-			String residencia, String naturalidade,Partido partido, char tipo) {
+	public CandidatoAR(String nome, int bi, String prof, Date dataNasc, String residencia,
+			String naturalidade,int partido, char tipo) {
 		super(nome, bi, prof, dataNasc, residencia, naturalidade);
 		this.partido = partido;
+		this.tipo = tipo;
+	}
+
+	public int getPartido() {
+		return partido;
+	}
+
+	public void setint(int partido) {
+		this.partido = partido;
+	}
+
+	public char getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(char tipo) {
 		this.tipo = tipo;
 	}
 }
