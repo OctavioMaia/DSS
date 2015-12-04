524f95af7ce96c5e92985c75d97b10e83af6654c
PPinto22 <pedropinto24@hotmail.com>
2015-12-04 16:53:08 +0000

    CandidatoAR
    Deixa de ter um Partido, passa a ter um inteiro, que é a chave

--- Projecto/src/Business/CandidatoAR.java
+++ Projecto/src/Business/CandidatoAR.java
@@ -2,33 +2,22 @@ package Business;
 
 import java.util.Date;
 
-<<<<<<< HEAD
-public class CandidatoAR extends Candidato{
-	
-	private int partido;
-	private char tipo;
-	
-	public CandidatoAR(String nome, int bi, String prof, Date dataNasc, String residencia,
-			String naturalidade,int partido, char tipo) {
-=======
 public class CandidatoAR extends Candidato {
 
-	private Partido partido;
+	private int partido;
 	private char tipo;
 
-	public CandidatoAR(String nome, int bi, String prof, Date dataNasc, String residencia, String naturalidade,Partido partido, char tipo) {
->>>>>>> refs/remotes/origin/master
+	public CandidatoAR(String nome, int bi, String prof, Date dataNasc, String residencia, String naturalidade,int partido, char tipo) {
 		super(nome, bi, prof, dataNasc, residencia, naturalidade);
 		this.partido = partido;
 		this.tipo = tipo;
 	}
 
-<<<<<<< HEAD
 	public int getPartido() {
 		return partido;
 	}
 
-	public void setint(int partido) {
+	public void setPartido(int partido) {
 		this.partido = partido;
 	}
 
@@ -39,11 +28,9 @@ public class CandidatoAR extends Candidato {
 	public void setTipo(char tipo) {
 		this.tipo = tipo;
 	}
-}
-=======
+
+	@SuppressWarnings("deprecation")
 	public String toString() {
 		return "Candidato " + super.getNome() + ", com BI nº"+super.getBi() + ", nascido a " + super.getDataNasc().toGMTString() + ", filiado ao partido " + partido;
 	}
-	
 }
->>>>>>> refs/remotes/origin/master
