6d2f6506e24b57f04d72e14c445fb70cec784d49
jms05 <joaosilva@Joaos-MacBook-Pro.local>
2015-12-04 16:04:07 +0000

    Elector DAO OK
    Acho que o eleitores DAO está ok

--- Projecto/src/Data/EleitoresDAO.java
+++ Projecto/src/Data/EleitoresDAO.java
@@ -5,8 +5,7 @@
  */
 package Data;
 
-import Bussines.Circulo;
-import Bussines.Eleitor;
+import Business.Eleitor;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -60,14 +59,15 @@ public class EleitoresDAO implements Map<Integer,Eleitor>{
 		} 
     }
     
-    public boolean containsKey(int bi){
+    @Override
+    public boolean containsKey(Object bi){
         boolean b=false;
         Connection conn = null;
         try{
         	conn = c.newConnection();
         	PreparedStatement ps = conn.prepareStatement(" Select  EXISTS (SELECT bi FROM Eleitores " +
                 " WHERE bi = ?)");
-        	ps.setInt(1, bi);
+        	ps.setInt(1,(Integer) bi);
         	ResultSet rs = ps.executeQuery();
         	while(rs.next())
         		b = (rs.getInt(1)!=0);
@@ -93,9 +93,9 @@ public class EleitoresDAO implements Map<Integer,Eleitor>{
         return b;
     }
     
-    
-    public boolean containsValue(Eleitor e){
-        return this.containsKey(e.getnIdent());
+    @Override
+    public boolean containsValue(Object e){
+        return this.containsKey(((Eleitor)e).getnIdent());
     }
     
     public boolean isEmpty(){
@@ -131,15 +131,15 @@ public class EleitoresDAO implements Map<Integer,Eleitor>{
         return ret;    
     }
 
-    
-    public Eleitor remove(Integer bi){
+    @Override
+    public Eleitor remove(Object bi){
     	Connection conn  = null;
     	Eleitor e =null;
     	try{
     		conn = this.c.newConnection();
     	    e  = this.get(bi); 
     	    PreparedStatement ps = conn.prepareStatement("DELETE FROM Eleitores where bi= ?");
-    	    ps.setInt(1, bi);
+    	    ps.setInt(1,(Integer) bi);
     	    ps.execute();
     	    conn.commit();
     	}catch(Exception e2){
@@ -192,14 +192,15 @@ public class EleitoresDAO implements Map<Integer,Eleitor>{
         return ret;
     }
     
-    public Eleitor get(int bi){
+    @Override
+    public Eleitor get(Object bi){
         Eleitor el  = null;
         Connection conn = null;
         
         try{
         	conn=this.c.newConnection();
         	PreparedStatement ps = conn.prepareStatement("Select * FROM Eleitores WHERE Bi = ?");
-        	ps.setInt(1, bi);
+        	ps.setInt(1, (Integer)bi);
         	ResultSet rs = ps.executeQuery();
             while(rs.next()){
                el = new Eleitor (rs.getString(2),rs.getInt(4),rs.getInt(1),rs.getInt(3));
@@ -241,26 +242,52 @@ public class EleitoresDAO implements Map<Integer,Eleitor>{
     }
   
     
-    public Eleitor put(Integer bi,  Eleitor e){
+    public Eleitor put(Integer bi,  Eleitor ep){
     	Connection conn=null;
+    	Eleitor el = this.remove(bi);
     	try{
     		conn = c.newConnection();
     		PreparedStatement ps = conn.prepareStatement("insert into Eleitores " +
                     "(BI,Nome,PIN,CirculoID) " +
                     "value " +
                     "(?,?,?,?)");
-    		ps.setString(2, e.getNome());
-            ps.setInt(1, e.getnIdent());
-            ps.setInt(3, e.getPin());
-            ps.setInt(4, e.getCirculo());
+    		ps.setString(2, ep.getNome());
+            ps.setInt(1, ep.getnIdent());
+            ps.setInt(3, ep.getPin());
+            ps.setInt(4, ep.getCirculo());
             ps.execute();
             ps.close();
             conn.commit();
+    	}catch(Exception e){
+    		try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+    	}finally{
+    		try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     	}
-        
-        
-        
-        
-	this.conn.commit();
+    	return el;
     }
+
+
+	@Override
+	public void putAll(Map<? extends Integer, ? extends Eleitor> m) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Funçao não implementada");
+		
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<Integer, Eleitor>> entrySet() {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Funçao não implementada");
+		//return null;
+	}
 }
