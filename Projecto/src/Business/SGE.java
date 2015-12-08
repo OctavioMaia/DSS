2b66cd08fe949ea37b171a6c0289608f015ca1f3
uNmasK95 <rui4825@gmail.com>
2015-12-08 11:06:52 +0000

    coligação DAO e sge

--- Projecto/src/Business/SGE.java
+++ Projecto/src/Business/SGE.java
@@ -3,14 +3,54 @@ package Business;
 import Data.CirculoDAO;
 import Data.PartidosDAO;
 import Data.ColigacaoDAO;
+import Data.Connector;
+import Data.EleicaoARDAO;
 import Data.EleicaoDAO;
+import Data.EleicaoPRDAO;
 
 public class SGE {
+	private Connector cn;
 	private PartidosDAO partidos;
 	private CirculoDAO circulos;
 	private ColigacaoDAO coligacoes;
-	private EleicaoDAO eleicoes;
-	private int ativa; 
+	private EleicaoPRDAO eleicoesPR;
+	private EleicaoARDAO eleicoesAR;
+	private ResultadoPRDAO resultadosPR;
+	private ResultadoARDAO resultadosAR;
+	private int ativa;
 	
+	
+	public SGE(){
+		this.cn = new Connector("jdbc:mysql://localhost/sge","root","12345");
+		try {
+			this.partidos = new PartidosDAO(cn);
+			this.circulos = new CirculoDAO(cn);
+			this.coligacoes = new ColigacaoDAO(cn);
+			this.eleicoesPR = new EleicaoPRDAO(cn);
+			this.eleicoesAR = new EleicaoARDAO(cn);
+			this.resultadosPR = new ResultadoPRDAO(cn);
+			this.resultadosAR = new ResultadoARDAO(cn);
+			this.ativa = procuraEleicaoAtiva();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private int procuraEleicaoAtiva(){return -1;}
+	
+	public boolean inserirCadernoRecenciamento(){return true;}
+	
+	public boolean iniciarEleicao(String id){return true;}
+	
+	public ResultadoPR verResultadosPR(String id){return new ResultadoPR(resultadosPR);}
+
+	public ResultadoAR verREsultadoAR(String id){return new ResultaAR(resultadosAR);}
+	
+	public void criarEleicaoAR(){}
+	
+	public void addListasPR(){}
+	
+	public void addListasAR(){}
 	
 }
