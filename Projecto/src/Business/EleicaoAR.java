a248aa8bc9c273f2567915d89dd518f0ec8c9c1d
PPinto22 <pedropinto24@hotmail.com>
2015-12-11 17:36:45 +0000

    penis

--- Projecto/src/Business/EleicaoAR.java
+++ Projecto/src/Business/EleicaoAR.java
@@ -7,54 +7,49 @@ import java.util.Map;
 import Data.CirculoInfoDAO;
 import Data.ResultadoCirculoARDAO;
 
-public class EleicaoAR extends Eleicao{
+public class EleicaoAR extends Eleicao {
 	private CirculoInfoDAO circulos;
 	private HashSet<Votavel> concorrentes;
 	private ResultadoCirculoARDAO resultado;
-	
-	/*
-	 * Este construtor inicia todos os resultados a 0.
-	 * É necessário, posteriormente,
-	 * atualizar o total de eleitores em cada círculo.
-	 */
-	public EleicaoAR(int idEleicao, Date data) {
+
+	public EleicaoAR(int idEleicao, Date data, Map<Integer, Integer> totEleitores) {
 		super(idEleicao, data);
 		this.circulos = new CirculoInfoDAO(idEleicao);
-		for(int i = 1; i<=22; i++){
-			if(!this.circulos.containsKey(i))
-			this.circulos.put(i,new CirculoInfo());
+		for (int i = 1; i <= 22; i++) {
+			if (!this.circulos.containsKey(i))
+				this.circulos.put(i, new CirculoInfo());
 		}
 		this.concorrentes = new HashSet<>();
 		this.resultado = new ResultadoCirculoARDAO(idEleicao);
-		for(int i = 1; i<=22; i++){
-			if(!this.resultado.containsKey(i))
-			this.resultado.put(i,new ResultadoCirculoAR());
+		for (int i = 1; i <= 22; i++) {
+			if (!this.resultado.containsKey(i))
+				this.resultado.put(i, new ResultadoCirculoAR(totEleitores.get(i)));
 		}
 	}
-	
-	public EleicaoAR(int idEleicao, Date data, Map<Integer,Integer> totEleitores){
+
+	public EleicaoAR(int idEleicao, Date data, Map<Integer, Integer> totEleitores, HashSet<Votavel> concorrentes) {
 		super(idEleicao, data);
 		this.circulos = new CirculoInfoDAO(idEleicao);
-		for(int i = 1; i<=22; i++){
-			if(!this.circulos.containsKey(i))
-			this.circulos.put(i,new CirculoInfo());
+		for (int i = 1; i <= 22; i++) {
+			if (!this.circulos.containsKey(i))
+				this.circulos.put(i, new CirculoInfo());
 		}
-		this.concorrentes = new HashSet<>();
+		this.concorrentes = concorrentes;
 		this.resultado = new ResultadoCirculoARDAO(idEleicao);
-		for(int i = 1; i<=22; i++){
-			if(!this.resultado.containsKey(i))
-			this.resultado.put(i,new ResultadoCirculoAR(totEleitores.get(i)));
+		for (int i = 1; i <= 22; i++) {
+			if (!this.resultado.containsKey(i))
+				this.resultado.put(i, new ResultadoCirculoAR(totEleitores.get(i)));
 		}
 	}
-	
-	public void setTotEleitores(Map<Integer,Integer> totEleitores){
-		for(int i: totEleitores.keySet()){
-			if(i>= 1 && i <= 22){
+
+	public void setTotEleitores(Map<Integer, Integer> totEleitores) {
+		for (int i : totEleitores.keySet()) {
+			if (i >= 1 && i <= 22) {
 				ResultadoCirculoAR rc = this.resultado.get(i);
 				rc.setTotEleitor(totEleitores.get(i));
-				this.resultado.put(i,rc);
+				this.resultado.put(i, rc);
 			}
 		}
 	}
-	
+
 }
