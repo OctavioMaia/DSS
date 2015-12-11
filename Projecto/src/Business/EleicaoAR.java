53c74cde5fb4b486c09ceea0fe2df0fc487e760d
PPinto22 <pedropinto24@hotmail.com>
2015-12-11 16:06:31 +0000

    penis

--- Projecto/src/Business/EleicaoAR.java
+++ Projecto/src/Business/EleicaoAR.java
@@ -2,6 +2,7 @@ package Business;
 
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Map;
 
 import Data.CirculoInfoDAO;
 import Data.ResultadoCirculoARDAO;
@@ -11,11 +12,35 @@ public class EleicaoAR extends Eleicao{
 	private HashSet<Votavel> concorrentes;
 	private ResultadoCirculoARDAO resultado;
 	
-	public EleicaoAR(int idEleicao, Date data, HashSet<Votavel> concorrentes) {
+	/*
+	 * Este construtor inicia todos os resultados a 0.
+	 * É necessário, posteriormente,
+	 * atualizar o total de eleitores em cada círculo.
+	 */
+	public EleicaoAR(int idEleicao, Date data) {
 		super(idEleicao, data);
 		this.circulos = new CirculoInfoDAO(idEleicao);
-		this.concorrentes = concorrentes;
+		for(int i = 1; i<=22; i++){
+			this.circulos.put(i,new CirculoInfo());
+		}
+		this.concorrentes = new HashSet<>();
 		this.resultado = new ResultadoCirculoARDAO(idEleicao);
+		for(int i = 1; i<=22; i++){
+			this.resultado.put(i,new ResultadoCirculoAR());
+		}
+	}
+	
+	public EleicaoAR(int idEleicao, Date data, Map<Integer,Integer> totEleitores){
+		super(idEleicao, data);
+		this.circulos = new CirculoInfoDAO(idEleicao);
+		for(int i = 1; i<=22; i++){
+			this.circulos.put(i,new CirculoInfo());
+		}
+		this.concorrentes = new HashSet<>();
+		this.resultado = new ResultadoCirculoARDAO(idEleicao);
+		for(int i = 1; i<=22; i++){
+			this.resultado.put(i,new ResultadoCirculoAR(totEleitores.get(i)));
+		}
 	}
 	
 	
