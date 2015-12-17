4c7c1828d801c0ff8bac6078e6cfa76954c7dd1a
OctavioMaia <octaviojmaia@gmail.com>
2015-12-17 00:49:26 +0000

    Revert "Add calendario decente"
    This reverts commit dff8f030c4211b9f5a3089ad39cb059c72529485.

--- Projecto/src/Presentation/TesteInterfaces.java
+++ Projecto/src/Presentation/TesteInterfaces.java
@@ -3,7 +3,7 @@ package Presentation;
 public class TesteInterfaces{
 	
 	public static void main(String args[]){
-		GerirPR i = new GerirPR(null);
+		//GerirPR i = new GerirPR();
 		//Login i = new Login(); feito-----------------------
 		//MainEleitor i = new MainEleitor();
 		//ResultadosAR i = new ResultadosAR();
