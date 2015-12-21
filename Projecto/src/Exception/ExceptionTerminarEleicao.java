2a1c7d88c6cfcc309943306f862182688fc9f6a3
uNmasK95 <rui4825@gmail.com>
2015-12-21 10:15:37 +0000

    foto no candidato Exceptions para o iniciar eleicao e o terminar eleicao

---
+++ Projecto/src/Exception/ExceptionTerminarEleicao.java
@@ -0,0 +1,6 @@
+package Exception;
+
+public class ExceptionTerminarEleicao extends Exception{
+	public ExceptionTerminarEleicao(){super();}
+	public ExceptionTerminarEleicao(String message){super(message);}
+}
